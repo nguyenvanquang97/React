c8b3319b776a740964a9305622b1c558f7aaf04f
nguyenvanquang97 <quangdtkc@gmail.com>
2022-11-03 13:58:14 +0700

    1

---
+++ User-management/src/api/userApi.jsx
@@ -0,0 +1,34 @@
+import axiosClient from "./axiosClient";
+
+const userApi = {
+  getUsers() {
+    const url = "/users";
+    return axiosClient.get(url);
+  },
+  createUser(newUser) {
+    const url = "/users";
+    return axiosClient.post(url, newUser);
+  },
+  updateUser(id, updatedUser) {
+    const url = `users/${id}`;
+    return axiosClient.put(url, updatedUser);
+  },
+  deleteUser(id) {
+    const url = `users/${id}`;
+    return axiosClient.delete(url);
+  },
+  searchUser(finUserName) {
+    const url = `users/search?name=${finUserName}`;
+    return axiosClient.get(url);
+  },
+  updatePassword(id,updatePasswordRequest) {
+    const url = `users/${id}/update-password`;
+    return axiosClient.put(url,updatePasswordRequest);
+  },
+  forgotPassword(id) {
+    const url = `/users/${id}/forgot-password`;
+    return axiosClient.post(url);
+  },
+};
+
+export default userApi;
